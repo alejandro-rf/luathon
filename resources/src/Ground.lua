bdd8df9c6a75f2c511be867c49a8347c93879b41
alejandro-rf <arenones@edu.tecnocampus.cat>
2021-11-19 00:15:23 +0100

    add ground

--- resources/src/Ground.lua
+++ resources/src/Ground.lua
@@ -1,27 +1,21 @@
 Actor = Actor or require "resources/lib/actor"
-local Coin = Actor:extend()
+local Ground = Actor:extend()
 
-local rotationSpeed = 0.5
-
-function Coin:new(x,y)
-  --Generate random spawn position
-  local randomImage = love.math.random(1, 4)
-  local randomSpawnPosX = love.math.random(0, 1280)
-  local randomSpawnPosY = love.math.random(800, 900)
+function Ground:new(x,y)
   self.delete = false
   
-  Coin.super.new(self,"resources/textures/coin.png",randomSpawnPosX, randomSpawnPosY, 50, 0 , -1)
+  Ground.super.new(self,"resources/textures/Ground.png",randomSpawnPosX, randomSpawnPosY, 50, 0 , -1)
 end
 
-function Coin:update(dt)
-    Coin.super.update(self,dt)
+function Ground:update(dt)
+    Ground.super.update(self,dt)
     --Check for borders
     self:borderCollision()
     --Rotation
     self.rot = self.rot + rotationSpeed * dt
 end
 
-function Coin:draw()
+function Ground:draw()
   local xx = self.position.x
   local ox = self.origin.x
   local yy = self.position.y
@@ -32,21 +26,4 @@ function Coin:draw()
   love.graphics.draw(self.image,xx,yy,rr,sx,sy,ox,oy,0,0)
 end
 
-function Coin:borderCollision()
-  if self.position.y < 0 - self.height then
-    --store pos?
-    self.delete = true
-  end
-end
-
-function Coin:resetPos()
-  --Generate new position
-	local randomXPos = love.math.random(0,800)
-  local randomYPos = love.math.random(0,600)
-  --Assign new position
-  self.position.x = randomXPos
-  self.position.y = randomYPos
-end
-
-
-return Coin
+return Ground
