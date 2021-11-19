e5b861c545a836cddd32958f79983e52ba65388e
alejandro-rf <arenones@edu.tecnocampus.cat>
2021-11-19 02:26:51 +0100

    update player texture

--- data.lua
+++ data.lua
@@ -3,7 +3,7 @@ local data = {}
 data.SCREEN_WIDTH, data.SCREEN_HEIGHT = love.graphics.getDimensions()
 
 -- player-related variables
-data.PLAYER_TEXTURE = "resources/textures/playerShip1_blue.png"
+data.PLAYER_TEXTURE = "resources/textures/animatedPlayer.png"
 data.COIN_TEXTURE = "resources/textures/coin.png"
 
 return data
